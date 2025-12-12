cf755f32eb6e1038bfffc7967b98e141346b037e
Duarte Cruz <duartecruz98@ik.me>
2025-12-12 02:33:05 +0000

    Add params file

--- launch/dronecan_driver.launch.py
+++ launch/dronecan_driver.launch.py
@@ -1,20 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('dronecan_driver'),
+        'config',
+        'params.yaml'
+    )
+
     return LaunchDescription([
         Node(
-            package='rm3100_driver',
-            executable='rm3100_driver.py',
-            name='rm3100_driver',
+            package='dronecan_driver',
+            executable='dronecan_driver.py',
+            name='dronecan_driver',
             output='screen',
-            parameters=[
-                {'port': 'can0'},
-                {'node_id': 124},
-                {'bitrate': 1000000},
-                {'topic': 'mag'},
-                {'frame_id': 'mag'},
-                {'publish_rate': 10}
-            ]
+            parameters=[config]
         )
     ])
